cfb1c2ad801a6044b1f11ee93be1fb718b1f73a2
Axel Guzmán <axl_bg@hotmail.com>
2025-01-15 14:30:10 -0300

    carta update

--- src/app/pages/carta/carta.component.ts
+++ src/app/pages/carta/carta.component.ts
@@ -20,15 +20,354 @@ export class CartaComponent {
       header: 'Desayunos y meriendas',
       items: [
         {
-          name: 'N°1 - Bebida caliente con dos medialunas',
+          name: 'N°1 - BEBIDA CALIENTE CON DOS MEDIALUNAS',
           description: '',
           price: 3500,
         },
         {
-          name: 'N°2 - Bebida caliente con tostadas',
+          name: 'N°2 - BEBIDA CALIENTE CON TOSTADAS',
           description:
-            'Acompañadas de manteca o queso crema y dulce de leche o mermelada',
+            'ACOMPAÑADAS DE MANTECA O QUESO CREMA Y DULCE DE LECHE O MERMELADA',
           price: 5700,
+        },
+        {
+          name: 'N°3 - NATURAL',
+          description:
+            'YOGURT con GRANOLA casera y FRUTOS SECOS Y FRUTA, acompañado con TOSTADAS, QUESO CREMA y MERMELADA',
+          price: 4200,
+        },
+        {
+          name: 'N°4 - BEBIDA CALIENTE CON 1/2 TOSTADO',
+          description: '',
+          price: 4500,
+        },
+        {
+          name: 'N°5 - LA CASA',
+          description:
+            'BEBIDA CALIENTE, mitad de JAMÓN/QUESO, mix de FRUTOS SECOS y jugo de NARANJA',
+          price: 4700,
+        },
+        {
+          name: 'N°6 - TOSTADO AMERICANO',
+          description:
+            'BEBIDA CALIENTE, TOSTADO AMERICANO y juguito de NARANJA',
+          price: 5900,
+        },
+      ],
+    },
+    {
+      header: 'Desayunos y meriendas',
+      items: [
+        {
+          name: 'N°7 - HUEVOS REVUELTOS',
+          description: 'BEBIDA CALIENTE, HUEVOS revueltos y juguito de NARANJA',
+          price: 6200,
+        },
+        {
+          name: 'N°8 - MERIENDA PARA DOS',
+          description:
+            '2 BEBIDAS CALIENTES, un cuadrado DULCE, 2 MEDIALUNAS, mitad TOSTADO y un SCON chico',
+          price: 13000,
+        },
+        {
+          name: 'N°9 - TOSTADO PARA DOS',
+          description: '2 BEBIDAS CALIENTES más un TOSTADO',
+          price: 9000,
+        },
+        {
+          name: 'N°10 - BRUNCH 1',
+          description:
+            '2 LATTES, 2 EXPRIMIDOS, 1 SCON RELLENO, HUEVOS REVUELTOS, 2 MEDIALUNAS, YOGURT con FRUTA y GRANOLA',
+          price: 19000,
+        },
+        {
+          name: 'N°11 - BRUNCH 2',
+          description:
+            '2 LATTES, 2 LIMONADAS, 2 MEDIALUNAS DE JAMÓN Y QUESO, 1 TORTA BROWNIE, 1 CUADRADO DULCE, 1 TOSTADO AMERICANO (pan de papa, jamón crudo y queso)',
+          price: 18000,
+        },
+      ],
+    },
+    {
+      header: 'Bebidas y Cafetería',
+      items: [
+        // Cafetería básica
+        {
+          name: 'RISTRETTO',
+          description: '',
+          price: 2000,
+        },
+        {
+          name: 'ESPRESSO',
+          description: '',
+          price: 2000,
+        },
+        {
+          name: 'ESPRESSO CORTADO',
+          description: '',
+          price: 2300,
+        },
+        {
+          name: 'ESPRESSO DOBLE',
+          description: '',
+          price: 2800,
+        },
+        {
+          name: 'DOBLE CORTADO',
+          description: '',
+          price: 3100,
+        },
+        {
+          name: 'CAFFE LATTE',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'CAFFE LATTE TAZÓN',
+          description: '',
+          price: 3200,
+        },
+        {
+          name: 'LÁGRIMA',
+          description: '',
+          price: 2300,
+        },
+        {
+          name: 'LAGRIMOTA',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'SUBMARINO',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'CHOCOLATADA',
+          description: '',
+          price: 2100,
+        },
+
+        // Cafés saborizados
+        {
+          name: 'CAFÉ SABORIZADO',
+          description: 'VAINILLA, CARAMELO, AVELLANAS',
+          price: 3000,
+        },
+        {
+          name: 'CAFÉ SABORIZADO EN TAZÓN',
+          description: 'VAINILLA, CARAMELO, AVELLANAS',
+          price: 3800,
+        },
+
+        // Cappuccinos
+        {
+          name: 'CAPPUCCINOS ITALIANO',
+          description: 'CHOCOLATE Y CANELA',
+          price: 3000,
+        },
+        {
+          name: 'CAPPUCCINOS EN TAZÓN',
+          description: '',
+          price: 3700,
+        },
+      ],
+    },
+    {
+      header: 'Cafés esperituosos',
+      items: [
+        {
+          name: 'CAFÉ CALIPSO',
+          description: '1 oz. de TÍA MARÍA, café espresso, crema y canela',
+          price: 4400,
+        },
+        {
+          name: 'CAFÉ IRLANDÉS',
+          description: '1 oz. de WHISKY, café espresso, crema y canela',
+          price: 4400,
+        },
+        {
+          name: 'VODKA ESPRESSO',
+          description: '1 oz. de VODKA, café espresso, crema y canela',
+          price: 4400,
+        },
+      ],
+    },
+    {
+      header: 'Bebidas Frías',
+      items: [
+        {
+          name: 'FRAPPUCHINO',
+          description: '',
+          price: 3900,
+        },
+        {
+          name: 'MILKSHAKE',
+          description: '',
+          price: 3700,
+        },
+        {
+          name: 'SMOOTHIES',
+          description: '',
+          price: 3700,
+        },
+      ],
+    },
+    {
+      header: 'Tés',
+      items: [
+        // Tés
+        {
+          name: 'TÉ NEGRO de CEYLON',
+          description: 'En saquitos',
+          price: 2000,
+        },
+        {
+          name: 'BLUE EARL GREY',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'BUENA VIBRA (CHAI)',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'ORANGE GREEN LOVE',
+          description: '',
+          price: 2500,
+        },
+        {
+          name: 'BE SWEET',
+          description: '',
+          price: 2500,
+        },
+      ],
+    },
+
+    {
+      header: 'Cosas Ricas',
+      items: [
+        {
+          name: 'MEDALUNAS',
+          description: '',
+          price: 700,
+        },
+        {
+          name: 'COOKIES',
+          description: '',
+          price: 1200,
+        },
+        {
+          name: 'FROLA de MEMBRILLO',
+          description: '',
+          price: 2400,
+        },
+        {
+          name: 'CHOCOTORTA',
+          description: '',
+          price: 3200,
+        },
+        {
+          name: 'CUADRADO de RICOTA',
+          description: '',
+          price: 2800,
+        },
+        {
+          name: 'BROWNIE con NUEZ',
+          description: '',
+          price: 2800,
+        },
+        {
+          name: 'APPLE CRUMBLE',
+          description: '',
+          price: 4100,
+        },
+        {
+          name: 'SCON DE LIMÓN',
+          description: '',
+          price: 2200,
+        },
+        {
+          name: 'ALFAJOR de NUEZ',
+          description: '',
+          price: 2200,
+        },
+        {
+          name: 'ALFAJOR de CHOCOLATE',
+          description: '',
+          price: 2200,
+        },
+        {
+          name: 'ALFAJOR de FRUTOS ROJOS',
+          description: '',
+          price: 2200,
+        },
+      ],
+    },
+
+    {
+      header: 'Cosas Ricas',
+      items: [
+        {
+          name: 'CHOCOTORTA',
+          description: '',
+          price: 2900,
+        },
+        {
+          name: 'TIRAMISÚ',
+          description: '',
+          price: 3700,
+        },
+        {
+          name: 'CHEESECAKE',
+          description: '',
+          price: 3900,
+        },
+        {
+          name: 'LEMON PIE',
+          description: '',
+          price: 3900,
+        },
+        {
+          name: 'TORTA BROWNIE',
+          description: '',
+          price: 3700,
+        },
+        {
+          name: 'PANES de QUESO',
+          description: '',
+          price: 3200,
+        },
+        {
+          name: 'SCON de QUESO',
+          description: '',
+          price: 3300,
+        },
+        {
+          name: 'SCON RELLENO',
+          description: '',
+          price: 4400,
+        },
+        {
+          name: 'MEDIALUNAS con JAMÓN y QUESO',
+          description: '',
+          price: 3400,
+        },
+        {
+          name: 'TOSTADO MEXICANO',
+          description: '',
+          price: 2800,
+        },
+        {
+          name: 'HUEVOS REVUELTOS',
+          description: '',
+          price: 3900,
+        },
+        {
+          name: 'TOSTADO',
+          description: '',
+          price: 4800,
         },
       ],
     },
